c3145d43b36ccd61ee08408dc3bbdf3446a6e53c
absentees <scottblissett@gmail.com>
2019-02-07 09:23:18 +1100

    WIP

--- screenshot.js
+++ screenshot.js
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
 import goodLib from "./good-lib";
-import CronJob from "cron";
 
 // module.exports = async function (req, res) {
 module.exports = async (req, res) => {
-	// try {
-		let cron = new CronJob('0 * * * * *', async function() {
-			console.log("Job is checking for good internet daily.");
-			//var cronJob = new CronJob('0 0 */12 * * *', function() { //  Production once a day cron job
-			// let websites = await goodLib.scrapeDesignerNews();
+	try {
+			let websites = await goodLib.scrapeDesignerNews();
+			console.log(websites.length);
 			// let topWebsite = await goodLib.sortWebsites(websites);
 			// topWebsite = await goodLib.getMeta(topWebsite);
 			// topWebsite = await goodLib.screenshot(topWebsite);
@@ -16,13 +13,9 @@ module.exports = async (req, res) => {
 			// topWebsite = await goodLib.addToAirtable(topWebsite);
 			// await goodLib.deleteLocalFiles(topWebsite);
 			// await goodLib.publishSite();
-		}, null, false, 'Australia/Sydney');
-	
-		cron.start();
-		console.log("Job is: " + cron.running + " – checking for good internet daily.");
-	// } catch (error) {
-	// 	console.error(error);
-	// }
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 
